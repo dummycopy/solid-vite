783eea657d5899ef1c5acb496253f2a28b69bef4
dummycopy <isjingxing@gmail.com>
2022-07-11 08:45:01 +0800

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { render } from 'solid-js/web';
 import App from './App';
+import {Router} from 'solid-app-router'
 import 'uno.css';
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(() =>
+<Router>
+<App />
+</Router>, document.getElementById('root') as HTMLElement);
